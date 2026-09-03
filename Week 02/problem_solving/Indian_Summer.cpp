b36157f554b83f94f92961c9697811af5cb5b2e1
Ahosan Habib Programmer <habibprogrammerbd@gmail.com>
2026-09-03 18:13:35 +0600

    one_problem_solve

--- Week 02/problem_solving/Indian_Summer.cpp
+++ Week 02/problem_solving/Indian_Summer.cpp
@@ -8,6 +8,33 @@ int main()
 
     int n;
     cin >> n;
-    
+    vector<pair<string, string>> v;
+    int con = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        bool x = false;
+
+        string a, b;
+        cin >> a >> b;
+
+        for (int j = 0; j < v.size(); j++)
+        {
+            if (v[j].first == a && v[j].second == b)
+            {
+                x = true;
+                con++;
+            }
+        }
+
+        if (x == false)
+        {
+            v.push_back({a, b});
+        }
+    }
+
+    cout << n - con << endl;
+
     return 0;
 }
